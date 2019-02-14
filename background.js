eebea28251a6ea51521e69a3f4e452b0208ca090
Jay Quigley <quigley.james.g@gmail.com>
2019-02-13 16:33:35 -0800

    Initial version of extension
    - Basic functionality: open new tab with correct URL for Wayback Machine
    - Introduce an icon

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({color: '#3aa757'}, () => {
-    console.log("The color is green.");
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+  chrome.browserAction.onClicked.addListener((tab) => {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
+      var url = tabs[0].url;
+      chrome.tabs.create({ url: `https://web.archive.org/web/*/${url}` });
+    });
   });
 });
