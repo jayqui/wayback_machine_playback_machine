c52de0e8783887c90b86f80e20ed67bdea37ead5
Jay Quigley <quigley.james.g@gmail.com>
2019-02-14 12:15:49 -0800

    stop listening for runtime.onInstalled

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
-      const url = tabs[0].url;
-      chrome.tabs.create({ url: `https://web.archive.org/web/*/${url}` });
-    });
+chrome.browserAction.onClicked.addListener((tab) => {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
+    const url = tabs[0].url;
+    chrome.tabs.create({ url: `https://web.archive.org/web/*/${url}` });
   });
 });
