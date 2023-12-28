01f9584ccc449cc23e759372bcfaab653c69f6a5
Jay Quigley <quigley.james.g@gmail.com>
2023-12-28 12:12:15 -0500

    Update to manifest v3
    Following docs at https://developer.chrome.com/docs/extensions/develop/migrate

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
     const url = tabs[0].url;
     chrome.tabs.create({ url: `https://web.archive.org/web/*/${url}` });
